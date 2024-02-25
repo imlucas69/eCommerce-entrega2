072bef57bd86eaaec26d141445a4c9c8d1c21867
Lucas Di Guglielmo <lucasdiguglielmo@gmail.com>
2024-02-25 16:29:04 -0300

    commit

---
+++ src/components/NabVar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>eCommerce</h3>
+            <div>
+                <button>Celulares</button>
+                <button>Vehiculos</button>
+                <button>Televisores</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
